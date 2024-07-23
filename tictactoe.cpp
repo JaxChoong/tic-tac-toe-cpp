5532f26ae146c4f54a01e31c16181d9fe9a98251
Jax Choong <choongjax@gmail.com>
2024-07-23 22:45:06 +0800

    feat: now uses boxIndex

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,20 +2,20 @@
 #include <string>
 
 int main(){
-  int boxes{9};
+  int boxes{8};
   int boxIndex[9]= {1,2,3,4,5,6,7,8,9};
   std::string line = "-------------\n| ";
 
-  for (int i = 1; i <= boxes; i++)
+  for (int i = 0; i <= boxes; i++)
   {
-    if (i%3)
+    if (boxIndex[i]%3)
     {
-      line += std::to_string(i) + " | ";
+      line += std::to_string(boxIndex[i]) + " | ";
     }
     else
     {
-      line += std::to_string(i) + " | \n";
-      if (i != 9)
+      line += std::to_string(boxIndex[i]) + " | \n";
+      if (boxIndex[i] != 9)
       {
         line += "----+---+----\n| ";
       }
